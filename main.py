454f225e56147a15090cec88a1d408d5f2453e5e
SerthK <khamsoda@yahoo.com>
2024-10-28 18:53:27 +0000

    Added character counter

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = get_book_wordcount(text)
-    
+    char_count = get_char_count(text)
+
     print(f"{count} words were found in the document.")
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +13,14 @@ def get_book_text(path):
     
 def get_book_wordcount(text):
         return len(text.split())
+
+def get_char_count(text):
+    chars = {}
+    for character in text:
+        if character.lower() in chars:
+            chars[character.lower()] += 1
+        else:
+            chars[character.lower()] = 1
+    return chars
 
 main()
